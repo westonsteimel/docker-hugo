24d76ddb345c7bd9f34a2a28a03789f0729e6ac1
Weston Steimel <weston.steimel@gmail.com>
2019-08-26 21:26:48 +0100

    use GitHub action for Docker Tag now that inspect is allowed
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- stable/tag.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-VERSION=$(grep -e "ARG HUGO_VERSION=" stable/Dockerfile)
-VERSION=${VERSION#ARG HUGO_VERSION=\"}
-VERSION=${VERSION%\"}
-echo "Tagging version ${VERSION}"
-docker tag "${DOCKER_USERNAME}/hugo:latest" "${DOCKER_USERNAME}/hugo:${VERSION}"
